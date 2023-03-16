2b88a5ee33413a34015da200b7b4525b3b885b40
Pavel Chirkov <pawelch@yandex.ru>
2023-03-16 15:01:07 +0300

    Дальше

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from dotenv.main import load_dotenv
+import os
+
+
+load_dotenv()
+API_PASSWORD = os.environ['API_PASSWORD']
 
 app = Flask(__name__)
 
+'''
+Заглавная страница api
+'''
 @app.route('/')
 def index():
-    return "Page"
+    if(ispassword(request.args.get("api_password"))):
+        return jsonify("Открытая страница")
+    else:
+        return jsonify("Доступ закрыт")
 
+'''
+Страница получения записи из группы
+'''
 @app.route('/get/note', methods=['GET'])
 def note():
     return "Last Note"
+
+
+'''
+Функция проверки доступа
+'''
+def ispassword(password):
+    if password == API_PASSWORD: 
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     app.run(debug=True)
