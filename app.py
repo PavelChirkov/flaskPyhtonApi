263aa160b312ad4c3cc6d46a8e19853448f9e397
Pavel Chirkov <pawelch@yandex.ru>
2023-03-17 20:01:36 +0300

    Работа с базой данных

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def index():
 '''
 Страница пользователя отображение
 '''
-
 @app.route('/user/view', methods=['GET'])
 def userView():
     if(ispassword(request.args.get("api_password"))):
@@ -47,27 +46,77 @@ def userView():
 '''
 Страница пользователя добавление
 '''
-
 @app.route('/user/add', methods=['GET'])
 def userAdd():
     if(ispassword(request.args.get("api_password"))):
         idUser = request.args.get("id_user")
         userName = request.args.get("username")
         timeStamp = datetime.fromtimestamp (time.time())
-        cursor.execute("INSERT INTO users (id_user, username,created_on) VALUES (%s, %s, %s)",(idUser,userName,timeStamp))
+        cursor.execute("INSERT INTO public.users (id_user, username,created_on) VALUES (%s, %s, %s)",(idUser,userName,timeStamp))
         conn.commit()
         return jsonify("Данные пользователя добавлены")
     else:
         return jsonify("Доступ закрыт")
 
+'''
+Страница получения данных группы
+'''
+@app.route('/group/view', methods=['GET'])
+def groupView():
+    if(ispassword(request.args.get("api_password"))):
+        id = request.args.get("id")
+        cursor.execute("SELECT * FROM public.groups where id=%s",(id))
+        records = cursor.fetchall()
+        return jsonify(records)
+    else:
+        return jsonify("Доступ закрыт")
 
 '''
-Страница получения записи из группы
+Страница добавление группы
+'''
+@app.route('/group/save', methods=['GET'])
+def groupSave():
+    if(ispassword(request.args.get("api_password"))):
+        idUser = request.args.get("id_user")
+        userName = request.args.get("username")
+        timeStamp = datetime.fromtimestamp (time.time())
+        cursor.execute("INSERT INTO public.groups (id_user, id_group, created_on, value, title) VALUES (%s, %s, %s)",(idUser,userName,timeStamp))
+        conn.commit()
+        return jsonify("Данные пользователя добавлены")
+    else:
+        return jsonify("Доступ закрыт")
+
+'''
+Страница получения сообщений группы
 '''
 
-@app.route('/get/note', methods=['GET'])
-def note():
-    return "Last Note"
+@app.route('/message/view', methods=['GET'])
+def groupMessage():
+    if(ispassword(request.args.get("api_password"))):
+        id = request.args.get("id")
+        cursor.execute("SELECT * FROM public.groups where id=%s",(id))
+        records = cursor.fetchall()
+        return jsonify(records)
+    else:
+        return jsonify("Доступ закрыт")
+
+'''
+Страница добавление сообщений группы
+'''
+@app.route('/message/save', methods=['GET'])
+def groupSave():
+    if(ispassword(request.args.get("api_password"))):
+        idUser = request.args.get("id_user")
+        idGroup = request.args.get("id_group")
+        value = request.args.get("value")
+        title = request.args.get("title")
+        timeStamp = datetime.fromtimestamp (time.time())
+        cursor.execute("INSERT INTO public.groups (id_user, id_group, created_on, value, title) VALUES (%s, %s, %s, %s, %s)",(idUser, idGroup, timeStamp, value, title))
+        conn.commit()
+        return jsonify("Данные пользователя добавлены")
+    else:
+        return jsonify("Доступ закрыт")
+
 
 '''
 Функция проверки доступа
